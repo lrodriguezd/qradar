5091b7dd75b45ca3cf46b9517e2fa131197960df
Luis Rodriguez Diaz <54954098+lrodriguezd@users.noreply.github.com>
2023-09-18 02:49:35 -0600

    Update run-script_nfs.sh

--- run-script_nfs.sh
+++ run-script_nfs.sh
@@ -15,7 +15,7 @@ while IFS= read -r line; do
         server_name=$(echo "$line" | awk '{print $2}')
 
         # Verificar si la línea no contiene localhost ni direcciones IPv6
-        if [[ "$ip_address" != "::"* && "$ip_address" != "127.0.0.1" && "$ip_address" != "0.0.0.0" && ! -z "$server_name" && ! "$line" =~ secondary ]]; then
+        if [[ "$ip_address" != "::"* && "$ip_address" != "127.0.0.1" && "$ip_address" != "0.0.0.0" && ! -z "$server_name" && ! "$line" =~ primary && ! "$line" =~ secondary ]]; then
             echo "Ejecutando $remote_script en $server_name ($ip_address)..."
 
             # Utiliza SSH para ejecutar el script en el servidor remoto
