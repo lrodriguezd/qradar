7fbacc68d67ccca0578561786f0449c001152b00
Luis Rodriguez Diaz <54954098+lrodriguezd@users.noreply.github.com>
2023-09-29 09:48:52 -0600

    Update run-script_nfs.sh

--- run-script_nfs.sh
+++ run-script_nfs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Ruta al archivo /etc/hosts
-hosts_file="/etc/hosts"
+# Ruta al archivo direcciones_ip.txt
+hosts_file="direcciones_ip.txt"
 
 # Nombre del script que deseas ejecutar en los servidores remotos
 remote_script="script_nfs.sh"
 
-# Leer el archivo /etc/hosts y ejecutar el script en servidores adecuados
+# Leer el archivo direcciones_ip.txt y ejecutar el script en los host
 while IFS= read -r line; do
     # Omitir las líneas que comienzan con #
     if [[ ! "$line" =~ ^\# ]]; then
-        # Obtener la dirección IP y el nombre del servidor desde la línea
+        # Obtener la dirección IP y el nombre del host desde la línea
         ip_address=$(echo "$line" | awk '{print $1}')
         server_name=$(echo "$line" | awk '{print $2}')
 
@@ -18,7 +18,7 @@ while IFS= read -r line; do
         if [[ "$ip_address" != "::"* && "$ip_address" != "127.0.0.1" && "$ip_address" != "0.0.0.0" && ! -z "$server_name" && ! "$line" =~ primary && ! "$line" =~ secondary ]]; then
             echo "Ejecutando $remote_script en $server_name ($ip_address)..."
 
-            # Utiliza SSH para ejecutar el script en el servidor remoto
+            # Utiliza SSH para ejecutar el script en el host
             ssh -o StrictHostKeyChecking=no "$server_name" "bash -s" < "$remote_script"
 
             echo "Script ejecutado en $server_name."
